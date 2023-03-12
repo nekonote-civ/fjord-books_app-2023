1fde78d44b1c9f5584960d345185acdfb48e60b8
nekonote <nekonote.civ@gmail.com>
2023-03-12 19:38:25 +0000

    ユーザーの登録後にユーザー詳細ページへリダイレクトするようにした

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
+  resources :users, only: [:show]
   resources :books
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
