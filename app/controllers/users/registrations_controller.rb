8a24168040cc361f9c82672294a82287142ee638
nekonote <nekonote.civ@gmail.com>
2023-03-13 10:57:36 +0000

    User 情報更新時に Profile も更新を行い、更新後は個別ページへリダイレクトするようにした

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,7 +2,7 @@
 
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
   def new
@@ -38,7 +38,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
 
   # def sign_up_params
   #   devise_parameter_sanitizer.sanitize(:sign_up) { |user| user.permit(permitted_attributes) }
@@ -50,12 +50,17 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [profile_attributes: [:id, :zipcode, :address, :text]])
+  end
 
   # The path used after sign up.
   def after_sign_up_path_for(resource)
+    user_path(resource)
+  end
+
+  # The path used after account update.
+  def after_update_path_for(resource)
     user_path(resource)
   end
 
