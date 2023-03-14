ba6fd115174470106fec8f89ea62cfdda465f46e
nekonote <nekonote.civ@gmail.com>
2023-03-14 13:29:21 +0000

    root の記述位置を変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
+  root 'books#index'
+
   devise_for :users, controllers: { registrations: 'users/registrations' }
   resources :users, only: [:index, :show]
   resources :books
 
-  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  root 'books#index'
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 end
