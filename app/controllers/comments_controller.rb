b995be65aff595e368aa0d120fa4a6ed474309f3
nekonote <nekonote.civ@gmail.com>
2023-03-23 13:48:17 +0000

    comments のバリデーション追加、および見た目の修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,16 +5,16 @@ class CommentsController < ApplicationController
 
   before_action :set_comment, only: %i[update destroy edit]
 
-  def edit; end
-
   def create
     @comment = @commentable.comments.build(comment_params)
     @comment.user_id = current_user.id
 
     if @comment.save
-      redirect_to request.referer, notice: 'Successfully posted a comment.'
+      redirect_to polymorphic_path(@comment.commentable), notice: 'Successfully posted a comment.'
     else
-      redirect_to request.referer, alert: 'Failed to post comment.'
+      redirect_to polymorphic_path(@comment.commentable), alert: "You don't have create to that comment.",
+                                                          status: :unprocessable_entity,
+                                                          flash: { errors: @comment.errors.full_messages }
     end
   end
 
@@ -29,10 +29,10 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-    return redirect_to request.referer, alert: "You don't have delete to that comment." unless created_by?(@comment.user_id)
+    return redirect_to polymorphic_path(@comment.commentable), alert: "You don't have delete to that comment." unless created_by?(@comment.user_id)
 
     @comment.destroy
-    redirect_to request.referer, notice: 'Comment was successfully destroyed.'
+    redirect_to polymorphic_path(@comment.commentable), notice: 'Comment was successfully destroyed.'
   end
 
   private
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,6 @@
 
 class Comment < ApplicationRecord
   belongs_to :commentable, polymorphic: true
+
+  validates :content, presence: true
 end
