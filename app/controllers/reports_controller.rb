8cf18b26f18c2e1c00b918ba5236ebdc6384313a
nekonote <nekonote.civ@gmail.com>
2023-03-27 06:35:18 +0000

    transaction を使用して mentions の登録/更新時に不整合が発生しないようにした

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -36,7 +36,7 @@ class ReportsController < ApplicationController
   def update
     @report.transaction do
       if @report.update(report_params)
-        @report.mentioning_relationships.each(&:destroy)
+        @report.mentioning_relationships.each(&:destroy!)
         create_mentions
         redirect_to @report, notice: t('controllers.common.notice_update', name: Report.model_name.human)
       else
@@ -65,17 +65,17 @@ class ReportsController < ApplicationController
     { mentioning_report_id:, mentioned_report_id: }
   end
 
-  def scan_mention_reports(content)
+  def create_mentions
+    mention_reports = scan_mentioning_reports(@report.content)
+    mention_reports.each do |mention_report|
+      Mention.create!(reports_params(@report.id, mention_report.id))
+    end
+  end
+
+  def scan_mentioning_reports(content)
     content.scan(REPORTS_REGEXP).map do |url|
       id = url.split('/').last
       Report.find_by(id:)
     end.compact.uniq
-  end
-
-  def create_mentions
-    mention_reports = scan_mention_reports(@report.content)
-    mention_reports.each do |mention_report|
-      Mention.create(reports_params(@report.id, mention_report.id))
-    end
   end
 end
